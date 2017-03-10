2bc222ff09555fa48fccf9ad98a9bf38c7e374b3
Joe Nyland <joenyland@me.com>
2017-03-10 19:05:19 +0000

    New tag and add head URL

--- Formula/docker-diff.rb
+++ Formula/docker-diff.rb
@@ -1,8 +1,9 @@
 class DockerDiff < Formula
   desc "Compare Docker images"
   homepage "https://github.com/JoeNyland/docker-diff"
-  url "https://github.com/JoeNyland/docker-diff.git"
+  url "https://github.com/JoeNyland/docker-diff/archive/v1.1.tar.gz"
   sha256 "21826141f9161f66a14f38c617a7f9c459d34439c47daf7029ea7a8ec83dc8c2"
+  head "https://github.com/JoeNyland/docker-diff.git"
   def install
     bin.install "docker-diff"
     bash_completion.install "docker-diff-completion"
