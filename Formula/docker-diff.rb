90f7cdeba17c5d9fdeefc7c3fb86e198e7f6def1
Joe Nyland <joenyland@me.com>
2017-03-10 18:16:49 +0000

    Install from my fork

--- Formula/docker-diff.rb
+++ Formula/docker-diff.rb
@@ -1,9 +1,8 @@
 class DockerDiff < Formula
   desc "Compare Docker images"
-  homepage "https://github.com/moul/docker-diff"
-  url "https://github.com/moul/docker-diff/archive/v1.0.tar.gz"
+  homepage "https://github.com/JoeNyland/docker-diff"
+  url "https://github.com/JoeNyland/docker-diff.git"
   sha256 "21826141f9161f66a14f38c617a7f9c459d34439c47daf7029ea7a8ec83dc8c2"
-  head "https://github.com/moul/docker-diff.git"
   def install
     bin.install "docker-diff"
   end
